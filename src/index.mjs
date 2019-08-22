bb996d29ec6c64c427b872b5ae57284edee45d02
Jiang Fengming <jiangfengming87@gmail.com>
2019-08-23 01:46:18 +0800

    createCastObject

--- src/index.mjs
+++ src/index.mjs
@@ -93,4 +93,94 @@ export function arrayOfString(a, defaults) {
   return a.map(i => string(i, defaults))
 }
 
+export function createCastObject(source) {
+  function getValue(key, isArray = false) {
+    let src = source
+
+    if (source.constructor === Function) {
+      src = source()
+    }
+
+    if (src.constructor === URLSearchParams) {
+      if (isArray) {
+        const val = src.getAll(key)
+
+        if (val.length === 1) {
+          return val[0].split(',')
+        } else {
+          return val
+        }
+      } else {
+        return src.get(key)
+      }
+    } else if (src.constructor === Object) {
+      if (isArray) {
+        const val = src[key]
+
+        if (val == null) {
+          return []
+        } else if (val.constructor === Array) {
+          return val
+        } else if (val.constructor === String) {
+          return val.split(',')
+        } else {
+          return []
+        }
+      } else {
+        const val = src[key]
+
+        if (val == null) {
+          return null
+        } else if (val.constructor === Array) {
+          return val[0]
+        } else if (val.constructor === String) {
+          return val
+        } else {
+          return null
+        }
+      }
+    }
+  }
+
+  return {
+    source,
+
+    int(key, radix, defaults, throwIfInvalid) {
+      return int(getValue(key), radix, defaults, throwIfInvalid)
+    },
+
+    float(key, defaults, throwIfInvalid) {
+      return float(getValue(key), defaults, throwIfInvalid)
+    },
+
+    number(key, defaults, throwIfInvalid) {
+      return number(getValue(key), defaults, throwIfInvalid)
+    },
+
+    bool(key, defaults, throwIfInvalid) {
+      return bool(getValue(key), defaults, throwIfInvalid)
+    },
+
+    string(key, defaults) {
+      return string(getValue(key), defaults)
+    },
+
+    arrayOfInt(key, radix, defaults, throwIfInvalid) {
+      return arrayOfInt(getValue(key, true), radix, defaults, throwIfInvalid)
+    },
+
+    arrayOfFloat(key, defaults, throwIfInvalid) {
+      return arrayOfFloat(getValue(key, true), defaults, throwIfInvalid)
+    },
+
+    arrayOfNumber(key, defaults, throwIfInvalid) {
+      return arrayOfNumber(getValue(key, true), defaults, throwIfInvalid)
+    },
+
+    arrayOfString(key, defaults, throwIfInvalid) {
+      return arrayOfString(getValue(key, true), defaults, throwIfInvalid)
+    }
+  }
+}
+
 export default { int, float, number, bool, string, arrayOfInt, arrayOfFloat, arrayOfNumber, arrayOfString }
